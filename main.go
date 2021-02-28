ac01bdc3a6b83257bfb07579e07aafe5e1de00bb
nanangqq <gingggg@gmail.com>
2021-02-28 06:09:34 +0000

    복습

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 	var results = make(map[string]string)
 	urls := []string{
 		"http://google.com",
+		"http://www.airbnb.com",
+		"http://www.reddit.com",
 	}
 	for _, url := range urls {
 		// fmt.Println(url)
@@ -24,14 +26,15 @@ func main() {
 	}
 	// results["hi"] = "hello"
 	// fmt.Println(results)
-	for _, result := range results {
-		fmt.Println(result)
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
 	fmt.Println("checking:", url)
 	res, err := http.Get(url)
+	fmt.Println(res.StatusCode)
 	if err != nil || res.StatusCode >= 400 {
 		return errRequestFailed
 	}
